0b429ecad819864aeee346d0c62d41c4365b9af5
Aki Kaivola <aki.kaivola@gmail.com>
2014-06-26 09:01:48 +0300

    update install script

--- install.sh
+++ install.sh
@@ -4,11 +4,16 @@ set -e
 
 cd $DIR_OF_SCRIPT
 
+git submodule init --update
 git submodule foreach git pull
 
 cd $HOME
 
-ln -sf $DIR_OF_SCRIPT/tmux.conf ./.tmux.conf
-ln -sf $DIR_OF_SCRIPT/oh-my-zsh/zshrc ./.zshrc
-ln -sf $DIR_OF_SCRIPT/vim-dot-files ~/.vim
-ln -sf ~/.vim/vimrc ~/.vimrc
+ln -sf $DIR_OF_SCRIPT/tmux.conf $HOME/.tmux.conf
+ln -sf $DIR_OF_SCRIPT/oh-my-zsh/zshrc $HOME/.zshrc
+ln -sf $DIR_OF_SCRIPT/vim-dot-files $HOME/.vim
+ln -sf $HOME/.vim/vimrc $HOME/.vimrc
+ln -sf $DIR_OF_SCRIPT/bin $HOME/bin
+ln -sf $DIR_OF_SCRIPT/ackrc $HOME/.ackrc
+ln -sf $DIR_OF_SCRIPT/gitconfig $HOME/.gitconfig
+ln -sf $DIR_OF_SCRIPT/emacs-live $HOME/.emacs.d
