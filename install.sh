92fa48de0e29124dbf3b93f3574f59a54c232011
Aki Kaivola <aki.kaivola@gmail.com>
2014-06-26 08:53:41 +0300

    submodules

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 DIR_OF_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -e
 
-cd ~
+cd $DIR_OF_SCRIPT
+
+git submodule foreach git pull
+
+cd $HOME
 
 ln -sf $DIR_OF_SCRIPT/tmux.conf ./.tmux.conf
 ln -sf $DIR_OF_SCRIPT/oh-my-zsh/zshrc ./.zshrc
+ln -sf $DIR_OF_SCRIPT/vim-dot-files ~/.vim
+ln -sf ~/.vim/vimrc ~/.vimrc
