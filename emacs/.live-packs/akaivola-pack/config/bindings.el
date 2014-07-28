d7334d4919129a796a1267e55e960a44ac48d2ac
Aki Kaivola <aki.kaivola@gmail.com>
2014-07-28 15:39:49 +0300

    evil-leader paredit bindings

--- emacs/.live-packs/akaivola-pack/config/bindings.el
+++ emacs/.live-packs/akaivola-pack/config/bindings.el
@@ -3,3 +3,12 @@
 ;; For example:
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+(evil-leader/set-leader ",")
+
+(evil-leader/set-key
+
+  "fs" 'paredit-forward-slurp-sexp
+  "bs" 'paredit-backward-slurp-sexp
+  "fb" 'paredit-forward-barf-sexp
+  "bb" 'paredit-backward-barf-sexp)
